179805711e75dc9c3171a8f3d06ee0912074011d
saurabh-gupta-ccg <saurabhcser8787@gmail.com>
2025-07-22 00:19:05 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = 4000;
+app.get("/", (req, res) => {
+  res.send("hello bhai");
+});
+app.get("/twitter", (req, res) => {
+  res.send("saurabhtwit@gupta");
+});
+app.get("/login",(req,res)=>{
+    res.send("go on the login page");
+})
+app.get("/saur",(req,res)=>{
+    res.send('<h1>this is saurabh page</h1>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`example app listening on port ${port}`);
+});
